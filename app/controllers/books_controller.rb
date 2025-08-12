778c38e93fa7423a82daff361aaabb68de397992
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-08-12 10:56:07 +0000

    [finish]new投稿機能実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+    private
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
---
+++ test/controllers/homes_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class HomesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get homes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get homes_create_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get homes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get homes_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get homes_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get homes_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get homes_destroy_url
+    assert_response :success
+  end
+end
